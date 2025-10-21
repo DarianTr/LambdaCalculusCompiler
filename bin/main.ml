05f06d60a1e8a07d986b5d392e60e75283e0097e
DarianTr <darian@tratar.de>
2025-10-21 17:05:15 +0200

    first parser draft

--- bin/main.ml
+++ bin/main.ml
@@ -1,15 +1,15 @@
 let rec print lexer = 
   let token = lexer#next_token in 
   match token with
-  | Lexer.LAMBDA -> print_endline "Lambda"; print lexer
-  | Lexer.RPAREN -> print_endline ")";  (print lexer)
-  | Lexer.LPAREN -> print_endline "(";(print lexer)
-  | Lexer.DOT-> print_endline "."; (print lexer)
-  | Lexer.VARIABLE name ->  print_endline ("Variable: " ^ name); (print lexer)
-  | Lexer.EOF ->  print_endline "End of file"; ""
+  | Frontend.Lexer.LAMBDA -> print_endline "Lambda"; print lexer
+  | Frontend.Lexer.RPAREN -> print_endline ")";  (print lexer)
+  | Frontend.Lexer.LPAREN -> print_endline "(";(print lexer)
+  | Frontend.Lexer.DOT-> print_endline "."; (print lexer)
+  | Frontend.Lexer.VARIABLE name ->  print_endline ("Variable: " ^ name); (print lexer)
+  | Frontend.Lexer.EOF ->  print_endline "End of file"; ""
 
 
 let () =
   let input = "\\x. x" in 
-  let lexer = new Lexer.lexer input (String.length input) in 
+  let lexer = new Frontend.Lexer.lexer input (String.length input) in 
   print_endline (print lexer)
