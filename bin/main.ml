af59c9f04d5708ff6b888ac76562d8e94fa594a8
DarianTr <darian@tratar.de>
2025-10-21 17:28:47 +0200

    fixed peek vs next_token bug

--- bin/main.ml
+++ bin/main.ml
@@ -1,15 +1,16 @@
-let rec print lexer = 
-  let token = lexer#next_token in 
-  match token with
-  | Frontend.Lexer.LAMBDA -> print_endline "Lambda"; print lexer
-  | Frontend.Lexer.RPAREN -> print_endline ")";  (print lexer)
-  | Frontend.Lexer.LPAREN -> print_endline "(";(print lexer)
-  | Frontend.Lexer.DOT-> print_endline "."; (print lexer)
-  | Frontend.Lexer.VARIABLE name ->  print_endline ("Variable: " ^ name); (print lexer)
-  | Frontend.Lexer.EOF ->  print_endline "End of file"; ""
-
+(* let rec print lexer =  *)
+(*   let token = lexer#next_token in  *)
+(*   match token with *)
+(*   | Frontend.Lexer.LAMBDA -> print_endline "Lambda"; print lexer *)
+(*   | Frontend.Lexer.RPAREN -> print_endline ")";  (print lexer) *)
+(*   | Frontend.Lexer.LPAREN -> print_endline "(";(print lexer) *)
+(*   | Frontend.Lexer.DOT-> print_endline "."; (print lexer) *)
+(*   | Frontend.Lexer.VARIABLE name ->  print_endline ("Variable: " ^ name); (print lexer) *)
+(*   | Frontend.Lexer.EOF ->  print_endline "End of file"; "" *)
+(**)
 
 let () =
   let input = "\\x. x" in 
   let lexer = new Frontend.Lexer.lexer input (String.length input) in 
-  print_endline (print lexer)
+  let parser = new Frontend.Parser.parser lexer in 
+  print_endline (Frontend.Parser.string_of_lambda parser#parse_expr)
