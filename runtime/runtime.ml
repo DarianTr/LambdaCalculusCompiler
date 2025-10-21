4a2b408cda104766f7d711f00b21bd1d8034ff4f
DarianTr <darian@tratar.de>
2025-10-22 01:19:38 +0200

    alpha is working but it sometimes skipps letters as naming + some no matches errors

--- runtime/runtime.ml
+++ runtime/runtime.ml
@@ -21,19 +21,21 @@ class runtime expr  =
     method replace_all var replacement expr = 
       match expr with
         | Frontend.Parser.Variable name -> self#replace_var name var replacement
-        | Frontend.Parser.Abstraction { var = Frontend.Parser.Var name; expr = body} -> if name = var then expr (*WATCH OUT SHADOWING, STOP APPLYING ALPHA ON THAT BRANCH*) 
-                                                                                else let new_expr = self#replace_all var replacement body in  Frontend.Parser.Abstraction{var = Frontend.Parser.Var(name); expr = new_expr}
-        | Frontend.Parser.Application { left = left; right = right} -> let l = self#replace_all var replacement left in let r = self#replace_all var replacement right in Frontend.Parser.Application{left = l; right = r}
+        | Frontend.Parser.Abstraction { var = Frontend.Parser.Var name; expr = body} -> if name = var then self#apply_alpha_conversion expr (*WATCH OUT SHADOWING, STOP APPLYING ALPHA ON THAT BRANCH*) 
+                                                                                else let new_expr = self#replace_all var replacement body in  let n = self#apply_alpha_conversion new_expr in  Frontend.Parser.Abstraction{var = Frontend.Parser.Var(name); expr = n} 
+        | Frontend.Parser.Application { left = left; right = right} -> 
+                              let l = self#replace_all var replacement left in let r = self#replace_all var replacement right in Frontend.Parser.Application{left = l; right = r}
         | _ -> expr 
     method apply_alpha_conversion cur_expr = 
        match cur_expr with
       | Frontend.Parser.Abstraction { var = Frontend.Parser.Var name; expr = body } -> 
         let new_name = num_to_letters (StringSet.cardinal list_of_variables)   in
-        let new_expr = (self#replace_all name new_name body) in 
-        let new_var = Frontend.Parser.Var(new_name) in
         list_of_variables <- StringSet.add new_name list_of_variables;
+        let new_expr = (self#replace_all name new_name body) in 
+        let n = self#apply_alpha_conversion new_expr in
+        let new_var = Frontend.Parser.Var(new_name) in
         print_endline new_name;
-        Frontend.Parser.Abstraction {var = new_var; expr = new_expr}
+        Frontend.Parser.Abstraction {var = new_var; expr = n}
 | Frontend.Parser.Application {left; right} -> let l = self#apply_alpha_conversion left in let r = self#apply_alpha_conversion right in Frontend.Parser.Application{left = l; right = r}
       | _ -> print_endline "no match"; cur_expr 
   end
