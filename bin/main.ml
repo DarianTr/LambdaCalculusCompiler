69f1bcda897fb030d9a2304d6b5cf4a1d6756503
DarianTr <darian@tratar.de>
2025-10-21 14:44:36 +0200

    proper lexer object now

--- bin/main.ml
+++ bin/main.ml
@@ -1,12 +1,15 @@
+let rec print lexer = 
+  let token = lexer#next_token in 
+  match token with
+  | Lexer.LAMBDA -> print_endline "Lambda"; print lexer
+  | Lexer.RPAREN -> print_endline ")";  (print lexer)
+  | Lexer.LPAREN -> print_endline "(";(print lexer)
+  | Lexer.DOT-> print_endline "."; (print lexer)
+  | Lexer.VARIABLE name ->  print_endline ("Variable: " ^ name); (print lexer)
+  | Lexer.EOF ->  print_endline "End of file"; ""
+
+
 let () =
   let input = "\\x. x" in 
-  let tokens = Lexer.tokenize input in 
-  List.iter (fun t -> 
-    match t with
-    | Lexer.LAMBDA -> print_endline "Lambda"
-    | Lexer.RPAREN -> print_endline ")"
-    | Lexer.LPAREN -> print_endline "("
-    | Lexer.DOT-> print_endline "."
-    | Lexer.VARIABLE name -> print_endline ("Variable: " ^ name)
-  ) tokens;
-
+  let lexer = new Lexer.lexer input (String.length input) in 
+  print_endline (print lexer)
--- lib/lexer.ml
+++ lib/lexer.ml
@@ -1,37 +1,48 @@
 
-
 type token =
   | LAMBDA (* '\' is used as a lambda symbol *) 
   | VARIABLE of string
   | LPAREN 
   | RPAREN
   | DOT
+  | EOF
 
 let is_variable_char c = 
   match c with
 | 'a'..'z' | 'A'..'Z'-> true
 | _ -> false
 
-let tokenize input = 
-  let len = String.length input in 
-  let rec aux i = 
-    if i >= len then []
-    else
-      match input.[i] with
-      | '\\' -> LAMBDA :: aux (i+1)  
-      | '(' -> LPAREN :: aux (i+1) 
-      | ')' -> RPAREN :: aux (i+1) 
-      | '.' -> DOT :: aux (i+1)
-      | c when is_variable_char c -> 
-        let rec aux2 j = 
-          if i + j > len then aux(i+j) 
-          else if i + j = len then  VARIABLE (String.sub input i j) :: aux (i + j) 
-          else 
-            match input.[i + j] with
-            | c when is_variable_char c -> aux2 (j+1)
-            | _ -> VARIABLE (String.sub input i j) :: aux (i + j)  
-        in 
-        aux2 0
-      | _ -> aux (i+1) (*skip unknown chars*)
-  in 
-  aux 0
+
+
+class lexer input len =  
+  object (self)
+    val mutable current_position: int = 0     
+    val mutable input: string = input 
+    val mutable len: int = len 
+    method peek = 
+        if  current_position >= String.length input then EOF 
+        else 
+          match input.[current_position] with
+           | '\\' -> LAMBDA 
+           | '(' -> LPAREN  
+           | ')' -> RPAREN
+           | '.' -> DOT 
+           | c when is_variable_char c -> 
+            let rec aux2 j = 
+            if current_position + j = len then  VARIABLE (String.sub input current_position j) 
+            else 
+              match input.[current_position + j] with
+              | c when is_variable_char c -> aux2 (j+1)
+              | _ -> VARIABLE (String.sub input current_position j)  
+            in 
+            aux2 0
+          | _ -> 
+            current_position <- current_position + 1;
+            self#peek
+  method next_token = 
+    let token = self#peek in 
+    match token with 
+      | VARIABLE name -> current_position <- current_position + (String.length name); token
+      | EOF -> token
+      | _ -> current_position <- current_position + 1; token
+end
